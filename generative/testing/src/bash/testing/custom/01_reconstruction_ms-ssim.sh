cb5ea515f64d76f4ec9d7691018bbfac5e304a39
Miretpl <miretpl@gmail.com>
2025-01-12 21:29:34 +0100

    Add bash scripts for result generation

---
+++ generative/testing/src/bash/testing/custom/01_reconstruction_ms-ssim.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+python /workspace/src/python/testing/compute_msssim_reconstruction.py \
+  --seed=42 \
+  --real_dir="/data/generation" \
+  --fake_dir="/generation/custom/results/reconstruction" \
+  --test_ids="/data/ids/raw/test.tsv" \
+  --batch_size=8 \
+  --num_workers=4
