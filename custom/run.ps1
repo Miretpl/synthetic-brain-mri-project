615bd79d785319392c9aa1795747ab1c55aec7e1
Miretpl <miretpl@gmail.com>
2025-01-12 12:29:52 +0100

    Add ps script for build and run docker container

---
+++ custom/run.ps1
@@ -0,0 +1,16 @@
+param(
+    [String]$dataPath,
+    [String]$modelPath
+)
+
+Write-Host "Docker image - build"
+docker build -t synthetic-brain-mri:custom-1.0.0 -f ./docker/Dockerfile .
+
+Write-Host "Docker image - run"
+docker run `
+    --rm `
+    --gpus all `
+    --ipc=host `
+    -v "${dataPath}:/data" `
+    -v "${modelPath}:/models" `
+    synthetic-brain-mri:custom-1.0.0
