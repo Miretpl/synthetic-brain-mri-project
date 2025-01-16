fc765fa09e59343320aab4d1ee2d0880497f6f20
Miretpl <miretpl@gmail.com>
2025-01-17 00:14:09 +0100

    [SEGMENTATION] Add ps for building and running docker image

---
+++ segmentation/run.ps1
@@ -0,0 +1,16 @@
+param(
+    [String]$dataPath,
+    [String]$modelsPath
+)
+
+Write-Host "Docker image - build"
+docker build . -f docker/Dockerfile -t synthetic-brain-mri:segmentation-1.0.0
+
+Write-Host "Docker image - run"
+docker run `
+    --rm `
+    --gpus all `
+    --ipc=host `
+    -v "${dataPath}:/data" `
+    -v "${modelsPath}:/models" `
+    synthetic-brain-mri:segmentation-1.0.0
