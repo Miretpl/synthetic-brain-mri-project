26b51cb3ca1b0f9b89fd44622997b32866de242a
Miretpl <miretpl@gmail.com>
2025-01-28 20:30:36 +0100

    [CUSTOM] Add generation data scripts for extra experiment

---
+++ generative/custom/src/bash/generation/seg/extra_exp/01_whole_train_set.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+dataset_quantity=( 8000 10000 12000 )
+for number in "${dataset_quantity[@]}"
+do
+  python src/generation.py \
+    --run_id=39 \
+    --output_dir="/data/segmentation/big/mixed/custom/dataset_$number" \
+    --ids_name="/data/ids/segmentation/big/mixed/exp_02/train_$number.tsv"
+done
