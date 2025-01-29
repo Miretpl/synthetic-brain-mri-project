58007609f5dfd6b7c20221cf2702e1a8c97b3a99
Miretpl <miretpl@gmail.com>
2025-01-29 21:07:48 +0100

    [SEG] Comment out training and evaluation model on extra experiment data

--- segmentation/docker/bash/01_training.sh
+++ segmentation/docker/bash/01_training.sh
@@ -29,41 +29,41 @@ python ./code/train.py \
   --real_data_path="/data/segmentation/controlnet"
 
 # Train segmentation model - BIG mixed datasets
-dataset_quantity=( 4000 6000 )
-for number in "${dataset_quantity[@]}"
-do
-  echo "Training model on big mixed datasets - custom - $number"
-  python ./code/train.py \
-    --output_dir="/models/exp_02/mixed/extra_exp/custom/ds_$number/runs" \
-    --train_ids="/data/ids/segmentation/big/mixed/exp_02/train_$number.tsv" \
-    --real_data_path="/data/raw/extracted" \
-    --fake_data_path="/data/segmentation/custom"
+#dataset_quantity=( 4000 6000 )
+#for number in "${dataset_quantity[@]}"
+#do
+#  echo "Training model on big mixed datasets - custom - $number"
+#  python ./code/train.py \
+#    --output_dir="/models/exp_02/mixed/extra_exp/custom/ds_$number/runs" \
+#    --train_ids="/data/ids/segmentation/big/mixed/exp_02/train_$number.tsv" \
+#    --real_data_path="/data/raw/extracted" \
+#    --fake_data_path="/data/segmentation/custom"
+#
+#  echo "Training model on big mixed datasets - controlnet - $number"
+#  python ./code/train.py \
+#    --output_dir="/models/exp_02/mixed/extra_exp/controlnet/ds_$number/runs" \
+#    --train_ids="/data/ids/segmentation/big/mixed/exp_02/train_$number.tsv" \
+#    --real_data_path="/data/raw/extracted" \
+#    --fake_data_path="/data/segmentation/controlnet"
+#done
 
-  echo "Training model on big mixed datasets - controlnet - $number"
-  python ./code/train.py \
-    --output_dir="/models/exp_02/mixed/extra_exp/controlnet/ds_$number/runs" \
-    --train_ids="/data/ids/segmentation/big/mixed/exp_02/train_$number.tsv" \
-    --real_data_path="/data/raw/extracted" \
-    --fake_data_path="/data/segmentation/controlnet"
-done
-
-dataset_quantity=( 8000 10000 12000 )
-for number in "${dataset_quantity[@]}"
-do
-  echo "Training model on big mixed datasets - custom - $number"
-  python ./code/train.py \
-    --output_dir="/models/exp_02/mixed/extra_exp/custom/ds_$number/runs" \
-    --train_ids="/data/ids/segmentation/big/mixed/exp_02/train_$number.tsv" \
-    --real_data_path="/data/raw/extracted" \
-    --fake_data_path="/data/segmentation/big/mixed/custom/dataset_$number"
-
-  echo "Training model on big mixed datasets - controlnet - $number"
-  python ./code/train.py \
-    --output_dir="/models/exp_02/mixed/extra_exp/controlnet/ds_$number/runs" \
-    --train_ids="/data/ids/segmentation/big/mixed/exp_02/train_$number.tsv" \
-    --real_data_path="/data/raw/extracted" \
-    --fake_data_path="/data/segmentation/big/mixed/controlnet/dataset_$number"
-done
+#dataset_quantity=( 8000 10000 12000 )
+#for number in "${dataset_quantity[@]}"
+#do
+#  echo "Training model on big mixed datasets - custom - $number"
+#  python ./code/train.py \
+#    --output_dir="/models/exp_02/mixed/extra_exp/custom/ds_$number/runs" \
+#    --train_ids="/data/ids/segmentation/big/mixed/exp_02/train_$number.tsv" \
+#    --real_data_path="/data/raw/extracted" \
+#    --fake_data_path="/data/segmentation/big/mixed/custom/dataset_$number"
+#
+#  echo "Training model on big mixed datasets - controlnet - $number"
+#  python ./code/train.py \
+#    --output_dir="/models/exp_02/mixed/extra_exp/controlnet/ds_$number/runs" \
+#    --train_ids="/data/ids/segmentation/big/mixed/exp_02/train_$number.tsv" \
+#    --real_data_path="/data/raw/extracted" \
+#    --fake_data_path="/data/segmentation/big/mixed/controlnet/dataset_$number"
+#done
 
 # Train segmentation model - SMALL REAL datasets
 dataset_quantity=( 20 40 60 80 100 200 400 600 800 )
