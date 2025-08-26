891ebe6a1c1ecac0d571a57d5ff624ff9e399e89
Miretpl <miretpl@gmail.com>
2025-08-26 19:30:32 +0200

    [GEN][SPADE] Refactor custom_dataset.py to match other generative model logic

--- generative/spade/src/code/data/custom_dataset.py
+++ generative/spade/src/code/data/custom_dataset.py
@@ -2,7 +2,8 @@
 Copyright (C) 2019 NVIDIA Corporation.  All rights reserved.
 Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
 """
-
+import pandas as pd
+from os.path import join
 from data.pix2pix_dataset import Pix2pixDataset
 from data.image_folder import make_dataset
 
@@ -24,27 +25,37 @@ class CustomDataset(Pix2pixDataset):
         parser.set_defaults(label_nc=13)
         parser.set_defaults(contain_dontcare_label=False)
 
-        parser.add_argument('--label_dir', type=str, required=True,
-                            help='path to the directory that contains label images')
-        parser.add_argument('--image_dir', type=str, required=True,
-                            help='path to the directory that contains photo images')
-        parser.add_argument('--instance_dir', type=str, default='',
-                            help='path to the directory that contains instance maps. Leave black if not exists')
+        parser.add_argument('--ids_path', type=str, required=True, help='Path to tsv file with set paths')
+        parser.add_argument('--data_dir', type=str, default=None, help='Path to directory where all data is stored')
+
         return parser
 
     def get_paths(self, opt):
-        label_dir = opt.label_dir
-        label_paths = make_dataset(label_dir, recursive=False, read_cache=True)
-
-        image_dir = opt.image_dir
-        image_paths = make_dataset(image_dir, recursive=False, read_cache=True)
-
-        if len(opt.instance_dir) > 0:
-            instance_dir = opt.instance_dir
-            instance_paths = make_dataset(instance_dir, recursive=False, read_cache=True)
+        if opt.ids_path is None:
+            data_dicts = [{
+                'flair': f'{opt.data_dir}/01045/03_flair_unhealthy_{idx}.png',
+                # This will be used as path for saving image
+                'seg': f'{opt.data_dir}/01045/03_seg_unhealthy.png'
+            } for idx in range(1000)]
         else:
-            instance_paths = []
+            df = pd.read_csv(opt.ids_path, sep='\t')
 
-        assert len(label_paths) == len(image_paths), "The #images in %s and %s do not match. Is there something wrong?"
+            if opt.data_dir is not None:
+                data_dicts = [
+                    {
+                        'flair': join(opt.data_dir, row['flair']),
+                        'seg': join(opt.data_dir, row['seg'])
+                    }
+                    for index, row in df.iterrows()
+                ]
+            else:
+                data_dicts = [
+                    {
+                        'flair': row['flair'],
+                        'seg': row['seg']
+                    }
+                    for index, row in df.iterrows()
+                ]
 
-        return label_paths, image_paths, instance_paths
+        print(f'Found {len(data_dicts)} subjects')
+        return data_dicts
