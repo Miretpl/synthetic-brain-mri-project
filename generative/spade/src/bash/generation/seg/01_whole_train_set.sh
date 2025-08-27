9d06140829751ecd80b880a1ab7773bd31891ad7
Miretpl <miretpl@gmail.com>
2025-08-27 22:10:12 +0200

    [GEN][SPADE] Add bash scripts for gen data for seg models

---
+++ generative/spade/src/bash/generation/seg/01_whole_train_set.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+python ./src/code/test.py \
+  --name 01 \
+  --preprocess_mode none \
+  --crop_size 224 \
+  --aspect_ratio 1.4 \
+  --label_nc 4 \
+  --contain_dontcare_label \
+  --output_nc 1 \
+  --data_dir /data/generation \
+  --ids_path /data/ids/raw/train.tsv \
+  --dataset_mode custom \
+  --no_instance \
+  --results_dir /data/segmentation/spade \
+  --which_epoch 50 \
+  --checkpoints_dir /models \
+  --no_save_samples
---
+++ generative/spade/src/bash/generation/seg/02_copy_seg_masks.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/bash
+
+python src/code/copy_data.py \
+  --src_dir="/data/raw/extracted" \
+  --dst_dir="/data/segmentation/spade" \
+  --ids="/data/ids/raw/train.tsv" \
+  --num_workers=8
