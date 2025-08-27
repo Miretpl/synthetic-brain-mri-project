882121ac0962837d6928f772bf43104774c164ba
Miretpl <miretpl@gmail.com>
2025-08-27 21:28:59 +0200

    [GEN][SPADE] Modify which_epoch type and add gen_type param

--- generative/spade/src/code/options/test_options.py
+++ generative/spade/src/code/options/test_options.py
@@ -10,8 +10,9 @@ class TestOptions(BaseOptions):
     def initialize(self, parser):
         BaseOptions.initialize(self, parser)
         parser.add_argument('--results_dir', type=str, default='./results/', help='saves results here.')
-        parser.add_argument('--which_epoch', type=str, default='latest', help='which epoch to load? set to latest to use latest cached model')
+        parser.add_argument('--which_epoch', type=int, help='which epoch to load?')
         parser.add_argument('--how_many', type=int, default=float("inf"), help='how many test images to run')
+        parser.add_argument('--gen_type', type=str, default=None, help='Generation type')
 
         parser.set_defaults(preprocess_mode='scale_width_and_crop', crop_size=256, load_size=256, display_winsize=256)
         parser.set_defaults(serial_batches=True)
--- generative/spade/src/code/test.py
+++ generative/spade/src/code/test.py
@@ -36,10 +36,6 @@ for i, data_i in enumerate(dataloader):
     generated = model(data_i, mode='inference')
 
     img_path = data_i['path']
-    for b in range(generated.shape[0]):
-        print('process image... %s' % img_path[b])
-        visuals = OrderedDict([('input_label', data_i['label'][b]),
-                               ('synthesized_image', generated[b])])
-        visualizer.save_images(webpage, visuals, img_path[b:b + 1])
 
-webpage.save()
+    for b in range(generated.shape[0]):
+        visualizer.save_images(opt, generated[b], img_path[b])
